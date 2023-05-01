45e9756622d420fa104ddeb34ef1d2173cc1b18c
Gustavo Fontana Vieira <83253065+gustavofontanavieira@users.noreply.github.com>
2023-05-01 09:20:55 -0300

    finalizado lógica do timer e nav

--- script.js
+++ script.js
@@ -3,66 +3,152 @@
 const timer = document.querySelector(".timer__counter--label");
 const timerBtn = document.querySelector(".timer__btn");
 const pomodoro1 = document.querySelectorAll(".nav__link[pomodoro]");
-const nav = document.querySelector(".nav");
-
-let pomodoroTime = 60 * 0.1;
-let breakTime = 60 * 5;
-let pomodoro2 = 60 * 50;
-let break2 = 60 * 10;
+const timerNav = document.querySelectorAll(".timer__nav--link[type]");
+const nextBtn = document.querySelector(".timer__counter--btn");
+const resetTimerLabel = document.querySelector(".timer__reset--label");
 
 let time;
-let breakNumber;
+let isCounting = false;
 
-let isCounting;
-/* 
-function calcTimer(timerN, breakN) {
-  if (isCounting === true) {
-    if (time == undefined || breakNumber == undefined) {
-      time = timerN;
-      breakNumber = breakN;
+let startTimer;
+
+// NAV ============================================================
+pomodoro1.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    resetTimer();
+
+    pomodoro1.forEach((item) => {
+      item.classList.remove("nav__link--active");
+    });
+    if (!e.target.classList.contains("nav__link--active")) {
+      e.target.classList.add("nav__link--active");
+
+      const pomodoroValue = e.target.attributes.pomodoro.value;
+      const timerNavActive = document.querySelector(
+        ".timer__nav--link--active"
+      );
+      if (pomodoroValue == 0) {
+        if (timerNavActive.attributes.type.value == 0) {
+          timer.textContent = "25:00";
+        } else {
+          timer.textContent = "05:00";
+        }
+      } else {
+        if (timerNavActive.attributes.type.value == 0) {
+          timer.textContent = "50:00";
+        } else {
+          timer.textContent = "10:00";
+        }
+      }
     }
+  });
+});
 
-    console.log("PARAAAAAAA");
+//Pomodoro timer NAV ==================================================
+timerNav.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const nowActive = document.querySelector(".nav__link--active");
+
+    resetTimer();
+
+    timerNav.forEach((item) => {
+      item.classList.remove("timer__nav--link--active");
+    });
+
+    if (!e.target.classList.contains("timer__nav--link--active")) {
+      e.target.classList.add("timer__nav--link--active");
+      const timerOrBreak = e.target.attributes.type.value;
+
+      if (nowActive.attributes.pomodoro.value == 0) {
+        timerOrBreak == 0
+          ? (timer.textContent = "25:00")
+          : (timer.textContent = "05:00");
+      } else {
+        timerOrBreak == 0
+          ? (timer.textContent = "50:00")
+          : (timer.textContent = "10:00");
+      }
+    }
+  });
+});
+
+//timer logic==========================================================
+function initializeValues(pomodoroValue) {
+  time = 60 * pomodoroValue;
+  calcTimer();
+  isCounting = true;
+}
+
+timerBtn.addEventListener("click", () => {
+  const pomodoroValue = document.querySelector(".nav__link--active");
+  const timerNavValue = document.querySelector(".timer__nav--link--active");
+  if (isCounting) {
+    console.log("Já esta contando");
+  } else {
+    if (pomodoroValue.attributes.pomodoro.value == 0) {
+      //25 minutos
+      if (timerNavValue.attributes.type.value == 0) {
+        initializeValues(25);
+      } else {
+        initializeValues(5);
+      }
+    } else {
+      //50 minutos
+      if (timerNavValue.attributes.type.value == 0) {
+        initializeValues(50);
+      } else {
+        initializeValues(10);
+      }
+    }
+  }
+});
+
+function calcTimer() {
+  if (time == undefined) {
+    return;
+  }
+  function tick() {
     const min = String(Math.trunc(time / 60)).padStart(2, 0);
     const sec = String(Math.trunc(time % 60)).padStart(2, 0);
 
     timer.textContent = `${min}:${sec}`;
 
-    if (time > 0) {
-      time--;
-    } else {
-      calcTimer(0, breakN);
-
+    console.log(startTimer);
+    if (time == 0) {
       clearInterval(startTimer);
       isCounting = false;
+    } else {
+      time--;
+    }
+  }
+
+  return (startTimer = setInterval(tick, 1000));
+}
+
+//btn next
+nextBtn.addEventListener("click", resetTimer);
+
+function resetTimer() {
+  clearInterval(startTimer);
+  isCounting = false;
+
+  const nowActive = document.querySelector(".nav__link--active");
+  const timerNavValue = document.querySelector(".timer__nav--link--active");
+
+  if (nowActive.attributes.pomodoro.value == 0) {
+    if (timerNavValue.attributes.type.value == 0) {
+      timer.textContent = "25:00";
+    } else {
+      timer.textContent = "05:00";
+    }
+  } else {
+    if (timerNavValue.attributes.type.value == 0) {
+      timer.textContent = "50:00";
+    } else {
+      timer.textContent = "10:00";
     }
   }
 }
 
-const startTimer = setInterval(() => {
-  calcTimer(pomodoro1, break1);
-  console.log("oe");
-}, 1000);
-
-timerBtn.addEventListener("click", () => {
-  if (isCounting) {
-    return;
-  } else {
-    startTimer;
-    isCounting = true;
-  }
-});
- */
-/* 
-console.log(nav.closest("nav nav"));
-
-pomodoro1.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    if (item.classList.contains("nav__link--active")) {
-      return;
-    } else {
-      item.classList.add("nav__link--active");
-    }
-  });
-});
- */
+// RESET TIMER
+resetTimerLabel.addEventListener("click", resetTimer);
